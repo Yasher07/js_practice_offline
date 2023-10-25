60033736c5c8f7d4b34409cd9c656f1f8aa37b7a
Yasher Arafath <86520621+Yasher07@users.noreply.github.com>
2023-10-25 12:39:38 +0530

    done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = "144553";
+let accountEmail = "yashera@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+console.log("accountId");
+
+console.table([accountId, accountPassword, accountCity]);
