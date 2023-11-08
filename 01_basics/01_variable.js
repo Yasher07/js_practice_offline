756d5cffb9f64b91ba3c47589cd5d64648812043
Yasher Arafath <86520621+Yasher07@users.noreply.github.com>
2023-11-08 11:20:21 +0530

    08-11-2023 morning

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -2,7 +2,13 @@ const accountId = "144553";
 let accountEmail = "yashera@gmail.com";
 var accountPassword = "12345";
 accountCity = "Jaipur";
+let accountState;
 
 console.log("accountId");
 
-console.table([accountId, accountPassword, accountCity]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hello");// will not work because we are using nodejs, not browser
+
+//code readability should be high
+
+let name = "yasher";
+let age = 25;
+let isLoggedIn = false;
+
+//PRIMITIVE DATATYPES-:
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // output is undefined
+console.log(typeof null); // output is object
